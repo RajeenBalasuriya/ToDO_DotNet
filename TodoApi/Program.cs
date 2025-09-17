f94373c1b34fb2c9ee3b99c71a0f4788bc63f5a4
RajeenBalasuriya <rajeenbalasuriya1@gmail.com>
2025-09-17 20:47:59 +0530

    created in memory db connection

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,25 +1,38 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
+
+// NSwag: generate an OpenAPI doc
+builder.Services.AddOpenApiDocument(settings =>
+{
+    settings.Title = "My API";
+    settings.Version = "v1";
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    // Serve the OpenAPI JSON (you can choose the path)
+    app.UseOpenApi(opt => opt.Path = "/openapi/v1.json");
+
+    // Serve Swagger UI (v3) and point it to that JSON
+    app.UseSwaggerUi(opt =>
+    {
+        opt.Path = "/swagger";                // UI at /swagger
+        opt.DocumentPath = "/openapi/v1.json"; // JSON at /openapi/v1.json
+    });
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
